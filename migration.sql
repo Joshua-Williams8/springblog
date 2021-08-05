7ccd3d3d271d4c939d6dd2dfc4fe9363cfdbc5fa
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-08-04 21:09:06 -0500

    updates

---
+++ migration.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS springblog_db;
+CREATE DATABASE springblog_db;
+
+USE springblog_db;
+
+# The structure should come from the models you pull, you should now run the project.
+
+
---
+++ seeder.sql
@@ -0,0 +1,22 @@
+USE springblog_db;
+
+# You will not beable to login to this user, unless you paste a hashed password  in here, but this is just to have some post displayed when you load the page.
+INSERT INTO springblog_db.users(username, email, password)
+values ('firstUser', 'user@email.com', 'notpassword');
+
+
+INSERT INTO springblog_db.ads(title, description, user_id)
+values ('I am a title', 'Max 255 characters.', 1),
+       ('Estoy Aqui', 'I am here in spanish', 1);
+
+# Not using categories currently so ignore lines below, this was for ads testing originally.
+# INSERT INTO springblog_db.categories(name)
+# values ('lorem'),
+#        ('ipsum'),
+#        ('ok');
+#
+# INSERT INTO springblog_db.ads_categories(ad_id, category_id)
+# VALUES (1,1),
+#        (1,2),
+#        (2,1),
+#        (2,3);
