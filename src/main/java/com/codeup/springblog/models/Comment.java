fffd3054674a6825d1eb8d886e1f4f7e1c478d7c
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-09-15 14:12:14 -0500

    review for work

---
+++ src/main/java/com/codeup/springblog/models/Comment.java
@@ -0,0 +1,23 @@
+package com.codeup.springblog.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+//We can also direct this to write to our database.
+@Entity
+@Table(name = "comments")
+public class Comment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(nullable = false, length = 200)
+  private String body;
+
+  @ManyToMany
+
+}
